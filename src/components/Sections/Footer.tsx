42c84822e77a242329a55a20c63c221c4ff1064c
Matsuel <matheolang@icloud.com>
2025-07-09 21:18:22 +0200

    feat: refactor le composant Footer pour améliorer la mise en page et supprimer les éléments inutilisés

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,13 +1,11 @@
 import { NAVBAR_LINKS } from '@/constants/navbar'
-import { MAIL, MAILTO, PHONES } from '@/constants/site'
 import Link from 'next/link'
-import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 
 const Footer = () => {
     return (
-        <footer className='w-full h-auto flex flex-col items-center justify-center bg-white shadow-2xl p-3 gap-4'>
-            <div className='w-[95%] h-auto flex flex-row items-center justify-center gap-4'>
+        <footer className='w-full h-auto flex flex-row items-center justify-between bg-white shadow-2xl p-3 gap-4 px-[10%]'>
+            <div className='w-auto h-auto flex flex-row items-center justify-start gap-8'>
                 {NAVBAR_LINKS.map((link) => (
                     <Link key={link.label} href={link.href} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base'>
                         {link.label}
@@ -18,20 +16,7 @@ const Footer = () => {
                 </Link>
             </div>
 
-            <div className='w-[95%] h-auto flex flex-row items-center justify-center gap-4'>
-                {PHONES.map((phone, index) => (
-                    <Link key={index} href={`tel:${phone.replace(/\s/g, '')}`} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base flex flex-row items-center gap-2'>
-                        <PhoneIcon width={20} height={20} />
-                        {phone}
-                    </Link>
-                ))}
-                <Link href={MAILTO} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base flex flex-row items-center gap-2'>
-                    <EnvelopeIcon width={20} height={20} />
-                    {MAIL}
-                </Link>
-            </div>
-
-            <div className='w-[95%] h-auto flex flex-row items-center justify-center'>
+            <div className='w-auto h-auto flex flex-row items-center justify-start'>
                 <p className='text-[#4A5565] font-medium text-base'>
                     &copy; {new Date().getFullYear()} Climenergy33. Tous droits réservés.
                 </p>
