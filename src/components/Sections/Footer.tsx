f33474b6baf4d428ef3a379549a5ffba72e8f575
Matsuel <matheolang@icloud.com>
2025-07-09 21:10:55 +0200

    feat: mise à jour du composant Footer avec des liens dynamiques et suppression de l'image

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,23 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
+import { NAVBAR_LINKS } from '@/constants/navbar'
+import { MAIL, MAILTO, PHONES } from '@/constants/site'
+import Link from 'next/link'
+import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+
 
 const Footer = () => {
     return (
-        <footer className='w-full h-auto flex flex-col items-center bg-white shadow-2xl p-3'>
-            <div className='w-[95%] h-auto flex flex-row items-center'>
-                <Image
-                    src="/logonobg.png"
-                    alt="Climenergy Logo"
-                    width={75}
-                    height={75}
-                    className='object-contain'
-                />
+        <footer className='w-full h-auto flex flex-col items-center justify-center bg-white shadow-2xl p-3 gap-4'>
+            <div className='w-[95%] h-auto flex flex-row items-center justify-center gap-4'>
+                {NAVBAR_LINKS.map((link) => (
+                    <Link key={link.label} href={link.href} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base'>
+                        {link.label}
+                    </Link>
+                ))}
+                <Link key={"Mentions Légales"} href={"/mentions-legales"} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base'>
+                    Mentions Légales
+                </Link>
             </div>
 
-            <div className='w-[95%] h-[1px] bg-gray-300 my-4' />
+            <div className='w-[95%] h-auto flex flex-row items-center justify-center gap-4'>
+                {PHONES.map((phone, index) => (
+                    <Link key={index} href={`tel:${phone.replace(/\s/g, '')}`} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base flex flex-row items-center gap-2'>
+                        <PhoneIcon width={20} height={20} />
+                        {phone}
+                    </Link>
+                ))}
+                <Link href={MAILTO} className='text-[#4A5565] hover:text-black transition-colors duration-300 font-medium text-base flex flex-row items-center gap-2'>
+                    <EnvelopeIcon width={20} height={20} />
+                    {MAIL}
+                </Link>
+            </div>
 
-            <div className='w-[95%] h-auto flex flex-row'>
-                <p className='text-gray-600 font-medium text-base'>
+            <div className='w-[95%] h-auto flex flex-row items-center justify-center'>
+                <p className='text-[#4A5565] font-medium text-base'>
                     &copy; {new Date().getFullYear()} Climenergy33. Tous droits réservés.
                 </p>
             </div>
