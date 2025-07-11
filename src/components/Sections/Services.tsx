3d15e7cb450bc4a42b56fe0a116fe3272fd29652
Matsuel <matheolang@icloud.com>
2025-07-11 19:36:39 +0200

    feat: amélioration de l'affichage des services avec gestion des états actifs

--- src/components/Sections/Services.tsx
+++ src/components/Sections/Services.tsx
@@ -10,12 +10,12 @@ const Services = () => {
         <div className='md:w-4/5 w-[90%] h-auto flex lg:flex-row flex-col items-stretch justify-center bg-white shadow-2xl p-3 gap-4 px-5 rounded-xl'>
             <div className="flex flex-col w-full lg:w-2/3 gap-4">
                 {SERVICES.map((service, index) => (
-                    <div key={index} className='w-full lg:w-1/3 h-auto flex flex-col items-start justify-start gap-4 p-4' onClick={() => setActiveService(index)}>
-                        <div className="w-auto h-auto flex flex-row items-center justify-start gap-2">
+                    <div key={index} className={`w-full lg:w-1/3 h-auto flex flex-col items-start justify-start gap-4 p-4 ${activeService === index ? 'bg-orange': 'bg-active-orange'} rounded-xl`} onClick={() => setActiveService(index)}>
+                        <div className={`w-auto h-auto flex flex-row items-center justify-start gap-2 ${activeService === index ? 'text-white' : 'text-black '}`}>
                             {service.icon}
-                            <h3 className='text-2xl font-bold'>{service.title}</h3>
+                            <h3 className={`text-2xl font-bold`}>{service.title}</h3>
                         </div>
-                        <p className={`text-gray-500 leading-6 ${activeService === index ? 'block' : 'hidden'}`}>{service.description}</p>
+                        <p className={`leading-6 ${activeService === index ? 'block' : 'hidden'} text-white`}>{service.description}</p>
                     </div>
                 ))}
             </div>
