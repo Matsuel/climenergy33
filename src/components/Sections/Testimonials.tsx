929aa8829033ddce75efb7ac455d695f89ec0a87
Matsuel <matheolang@icloud.com>
2025-07-12 22:35:01 +0200

    fix: ajustement de la mise en page du composant Testimonials pour une meilleure répartition des éléments

--- src/components/Sections/Testimonials.tsx
+++ src/components/Sections/Testimonials.tsx
@@ -5,8 +5,8 @@ import Testimonial from '../Testimonial'
 const Testimonials = () => {
     return (
         <div className='md:w-4/5 w-[90%] h-auto flex flex-row items-start justify-between bg-white shadow-2xl p-4 gap-4 px-5 rounded-xl'>
-            <div className='w-1/2 h-auto flex items-center justify-center'>
-                <div className="w-full h-[600px] overflow-hidden rounded-lg flex p-4">
+            <div className='w-1/4 h-auto flex items-center justify-center'>
+                <div className="w-full h-[300px] overflow-hidden rounded-lg flex p-4">
                     <Image
                         src="/3clims.jpeg"
                         alt="Témoignages"
@@ -16,7 +16,7 @@ const Testimonials = () => {
                     />
                 </div>
             </div>
-            <div className='w-1/2 h-auto flex flex-col items-start justify-start gap-2'>
+            <div className='w-3/4 h-auto flex flex-col items-start justify-start gap-2'>
                 <h3 className='text-4xl font-bold'>L&apos;avis de nos clients - Votre confort est notre priorité !</h3>
                 <Testimonial
                     stars={5}
