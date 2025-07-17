204e4ff4d3c470bb96d812cd66003fddcef863a3
Matsuel <matheolang@icloud.com>
2025-07-17 21:26:03 +0200

    fix: ajustement de la mise en page et ajout des boutons de contact dans le composant Contact

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -12,55 +12,58 @@ const Contact = () => {
     const router = useRouter();
 
     return (
-        <div className='md:w-4/5 w-[90%] h-auto flex lg:flex-row flex-col justify-center items-start rounded-xl gap-4'>
+        <div className='md:w-4/5 w-[90%] h-auto flex lg:flex-row flex-col justify-center items-stretch rounded-xl gap-4'>
 
-                <div className='w-full lg:w-1/2 h-auto p-4 flex flex-col items-start justify-start gap-4'>
+            <div className='w-full lg:w-1/2 h-auto p-4 flex flex-col items-start justify-between gap-4'>
+                <div className='w-full h-auto flex flex-col gap-4'>
                     <h3 className='text-5xl font-bold'>Nous contacter</h3>
                     <p className='text-xl text-gray-500 leading-6'>
                         Un problème, une question, une demande de devis ou un renseignement ?
                         N&apos;hésitez pas à nous contacter, nous sommes là pour vous aider !
                     </p>
-
-                    <div className='w-full h-auto flex flex-col gap-4'>
-                        {PHONES.map((phone, index) => (
-                            <ContactButton
-                                key={index}
-                                icon={<PhoneIcon width={24} height={24} className='text-gray-400' />}
-                                title="Téléphone"
-                                content={phone}
-                                onClick={() => router.push(`tel:${phone}`)}
-                            />
-                        ))}
-                        <ContactButton
-                            icon={<EnvelopeIcon width={24} height={24} className='text-gray-400' />}
-                            title="Email"
-                            content={MAIL}
-                            onClick={() => router.push(MAILTO)}
-                        />
-                        <ContactButton
-                            icon={<BuildingOffice2Icon width={24} height={24} className='text-gray-400' />}
-                            title="Adresse"
-                            content={ADDRESS}
-                        />
-                    </div>
                 </div>
 
-                <div className='w-full lg:w-1/2 h-auto p-4 flex flex-col items-start justify-center gap-2'>
-
-                    <div className='w-full h-auto bg-white flex flex-col gap-4 p-4 rounded-lg shadow-2xl'>
-                        <div className='w-full h-auto flex flex-row items-center justify-between gap-4'>
-                            <Input type="text" label='Nom*' />
-                            <Input type="text" label='Prénom' />
-                        </div>
-                        <Input type="text" label='Téléphone*' />
-                        <Input type="email" label='Email*' />
-                        <TextArea label='Votre message*' />
-                        <Button variant='primary' className='text-sm'>Envoyer</Button>
-                    </div>
+                <div className='w-full h-auto flex flex-col gap-2'>
+                    <h4 className='text-2xl font-semibold'>Nos coordonnées</h4>
+                    {PHONES.map((phone, index) => (
+                        <ContactButton
+                            key={index}
+                            icon={<PhoneIcon width={24} height={24} className='text-gray-400' />}
+                            title="Téléphone"
+                            content={phone}
+                            onClick={() => router.push(`tel:${phone}`)}
+                        />
+                    ))}
+                    <ContactButton
+                        icon={<EnvelopeIcon width={24} height={24} className='text-gray-400' />}
+                        title="Email"
+                        content={MAIL}
+                        onClick={() => router.push(MAILTO)}
+                    />
+                    <ContactButton
+                        icon={<BuildingOffice2Icon width={24} height={24} className='text-gray-400' />}
+                        title="Adresse"
+                        content={ADDRESS}
+                    />
                 </div>
-
-
             </div>
+
+            <div className='w-full lg:w-1/2 h-auto p-4 flex flex-col items-start justify-center gap-2'>
+
+                <div className='w-full h-auto bg-white flex flex-col gap-4 p-4 rounded-lg shadow-2xl'>
+                    <div className='w-full h-auto flex flex-row items-center justify-between gap-4'>
+                        <Input type="text" label='Nom*' />
+                        <Input type="text" label='Prénom' />
+                    </div>
+                    <Input type="text" label='Téléphone*' />
+                    <Input type="email" label='Email*' />
+                    <TextArea label='Votre message*' />
+                    <Button variant='primary' className='text-sm'>Envoyer</Button>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
