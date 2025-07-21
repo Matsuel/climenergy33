f30090a8a52b1f22e178a1c46a052de1cbd6e63b
Matsuel <matheolang@icloud.com>
2025-07-21 21:31:45 +0200

    Caroussel V1

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -11,7 +11,7 @@ const Dot = ({
     ...props
 }: DotProps) => {
     return (
-        <div className={cn(`w-4 h-4 bg-transparent rounded-full border-2 ${isActive ? 'border-black' : 'border-gray-400'} flex items-center justify-center`, className)}>
+        <div className={cn(`w-4 h-4 bg-transparent rounded-full border-2 ${isActive ? 'border-black' : 'border-gray-400'} flex items-center justify-center`, className)} {...props}>
             <div className={cn('w-2 h-2 rounded-full', {
                 'bg-black': isActive,
                 'bg-transparent': !isActive
