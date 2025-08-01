e87f031d48e602b216a08178a56708a3cdfc972b
Matsuel <matheolang@icloud.com>
2025-08-01 21:42:45 +0200

    feat: supprimer les dégradés de fond dans le composant Réalisation

--- src/components/Sections/Realisation.tsx
+++ src/components/Sections/Realisation.tsx
@@ -39,8 +39,6 @@ const Realisation = () => {
                     <ReviewCard key={review} src={review} />
                 ))}
             </Marquee>
-            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
         </Layout>
     )
 }
