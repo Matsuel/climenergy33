d75ddf4bd8f8ca0d72f1ddab466ecac9286f3cd3
Matsuel <matheolang@icloud.com>
2025-08-01 22:25:13 +0200

    feat: remplacer le composant Testimonial par TestimonialsColumn pour une meilleure gestion des témoignages

--- src/components/Sections/Avis.tsx
+++ src/components/Sections/Avis.tsx
@@ -1,5 +1,66 @@
-import Layout from "../Layout"
-import Testimonial from "../Testimonial"
+import Layout from "../Layout";
+import TestimonialsColumn from "../TestimonialColumn";
+
+const testimonials = [
+    {
+        text: "This ERP revolutionized our operations, streamlining finance and inventory. The cloud-based platform keeps us productive, even remotely.",
+        image: "/logo.png",
+        name: "Briana Patton",
+        role: "Operations Manager",
+    },
+    {
+        text: "Implementing this ERP was smooth and quick. The customizable, user-friendly interface made team training effortless.",
+        image: "/logo.png",
+        name: "Bilal Ahmed",
+        role: "IT Manager",
+    },
+    {
+        text: "The support team is exceptional, guiding us through setup and providing ongoing assistance, ensuring our satisfaction.",
+        image: "/logo.png",
+        name: "Saman Malik",
+        role: "Customer Support Lead",
+    },
+    {
+        text: "This ERP's seamless integration enhanced our business operations and efficiency. Highly recommend for its intuitive interface.",
+        image: "/logo.png",
+        name: "Omar Raza",
+        role: "CEO",
+    },
+    {
+        text: "Its robust features and quick support have transformed our workflow, making us significantly more efficient.",
+        image: "/logo.png",
+        name: "Zainab Hussain",
+        role: "Project Manager",
+    },
+    {
+        text: "The smooth implementation exceeded expectations. It streamlined processes, improving overall business performance.",
+        image: "/logo.png",
+        name: "Aliza Khan",
+        role: "Business Analyst",
+    },
+    {
+        text: "Our business functions improved with a user-friendly design and positive customer feedback.",
+        image: "/logo.png",
+        name: "Farhan Siddiqui",
+        role: "Marketing Director",
+    },
+    {
+        text: "They delivered a solution that exceeded expectations, understanding our needs and enhancing our operations.",
+        image: "/logo.png",
+        name: "Sana Sheikh",
+        role: "Sales Manager",
+    },
+    {
+        text: "Using this ERP, our online presence and conversions significantly improved, boosting business performance.",
+        image: "/logo.png",
+        name: "Hassan Ali",
+        role: "E-commerce Manager",
+    },
+];
+
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
 
 const Avis = () => {
     return (
@@ -9,25 +70,13 @@ const Avis = () => {
             id='temoignages'
             className='w-full min-h-auto flex flex-col items-center gap-4'
         >
-            <div className="flex flex-row items-center justify-center flex-wrap gap-4">
-                <Testimonial
-                    author="John Doe"
-                    text='This is a great service! Highly recommend.'
-                    image='/logo.png'
-                    stars={5}
-                    variant="xl"
-                />
-                <Testimonial
-                    author="Jane Smith"
-                    text='Absolutely loved the experience. Will use again!'
-                    stars={4}
-                />
-                <Testimonial
-                    author="Alice Johnson"
-                    text='Very satisfied with the quality and service.'
-                    image='/logo.png'
-                    stars={5}
-                />
+            <div className="container z-10 mx-auto">
+
+                <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[740px] overflow-hidden">
+                    <TestimonialsColumn testimonials={firstColumn} duration={15} />
+                    <TestimonialsColumn testimonials={secondColumn} className="hidden md:block" duration={19} />
+                    <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:block" duration={17} />
+                </div>
             </div>
         </Layout>
     )
