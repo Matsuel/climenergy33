8886c4d623707230531bd9fe28be4b88c2b91cff
Matsuel <matheolang@icloud.com>
2025-08-04 21:30:16 +0200

    feat: améliorer la fonction POST pour envoyer des emails avec des données dynamiques

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,22 +4,32 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req: Request) {
+
+    const body = await req.json();
+
+    const { firstName, lastName, email, phone, message } = body;
+
     try {
         const { data, error } = await resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
-            to: [''], // Adresse e-mail du destinataire, et en copie celle de ce33 pour répondre au client
+            to: ['matsuel0308@gmail.com'], // Adresse e-mail du destinataire, et en copie celle de ce33 pour répondre au client
             // cc: [MAIL],
             subject: 'Hello world',
-            react: EmailTemplate({ firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', message: 'Hello, this is a test message.' }),
+            react: EmailTemplate({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                phone: phone,
+                message: message,
+            }),
         });
 
         if (error) {
-            return Response.json({ error }, { status: 500 });
+            return Response.json({ error }, { status: 500});
         }
-
-        return Response.json(data);
+        return Response.json({ success: true, message: 'Email sent successfully', data }, { status: 200 });
     } catch (error) {
-        return Response.json({ error }, { status: 500 });
+        return Response.json({ error }, { status: 500});
     }
 }
